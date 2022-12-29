194de560c7ad30c13e33ad0245b29272299292ee
PradnyaTipare <pradnyatipare04@gmail.com>
2022-12-28 17:57:45 -0700

    Update Case Study 6

--- Case Study 6 - Clique Bait/Digital Analysis.sql
+++ Case Study 6 - Clique Bait/Digital Analysis.sql
@@ -1,78 +1,78 @@
 -- How many users are there?
-select count(distinct(user_id)) as total_users 
-from users
+SELECT COUNT(DISTINCT(user_id)) AS total_users 
+FROM users
 
 -- How many cookies does each user have on average?
-with cookie_count_cte as (
-select user_id,
-	count(distinct cookie_id) as cookie_count 
-from users 
-group by user_id)
-select round(avg(cookie_count),2) as avg_cookies 
-from cookie_count_cte
+WITH cookie_COUNT_cte AS (
+SELECT user_id,
+	COUNT(DISTINCT cookie_id) AS cookie_COUNT 
+FROM users 
+GROUP BY user_id)
+SELECT ROUND(AVG(cookie_COUNT),2) AS AVG_cookies 
+FROM cookie_COUNT_cte
 
--- What is the unique number of visits by all users per month?
-select month(event_time) as month,
-	count(distinct(visit_id)) as unique_visits 
-from events 
-group by month(event_time)
+-- What is the unique number of visits by all users per MONTH?
+SELECT MONTH(event_time) AS MONTH,
+	COUNT(DISTINCT(visit_id)) AS unique_visits 
+FROM events 
+GROUP BY MONTH(event_time)
 
 -- What is the number of events for each event type?
-select ei.event_type,
+SELECT ei.event_type,
 	event_name,
-	count(*) 
-from events e inner join event_identifier ei on e.event_type = ei.event_type 
-group by event_type,event_name
+	COUNT(*) 
+FROM events e INNER JOIN event_identifier ei ON e.event_type = ei.event_type 
+GROUP BY event_type,event_name
 
 -- What is the percentage of visits which have a purchase event?
-with visits_cte as (select event_name,
-	count(distinct visit_id) as visits
-from events e inner join event_identifier ei on e.event_type = ei.event_type 
-group by event_name)
-select round(100 * max(case when event_name = 'Purchase' then visits end)/sum(visits),2)
-from visits_cte
+WITH visits_cte AS (SELECT event_name,
+	COUNT(DISTINCT visit_id) AS visits
+FROM events e INNER JOIN event_identifier ei ON e.event_type = ei.event_type 
+GROUP BY event_name)
+SELECT ROUND(100 * MAX(CASE WHEN event_name = 'Purchase' THEN visits END)/SUM(visits),2)
+FROM visits_cte
 
 
 -- What is the percentage of visits which view the checkout page but do not have a purchase event?
-with visit_cte as (
-select visit_id,
-	page_id as checkout,
-	lead(page_id) over(partition by visit_id order by sequence_number) as purchase
-from events
-where page_id in ('12','13') 
-order by cookie_id,event_time)
-select round(100 * sum(case when checkout = 12 and purchase is null then 1 else 0 end)/count(distinct(visit_id)),2) as per_visits
-from visit_cte
+WITH visit_cte AS (
+SELECT visit_id,
+	page_id AS checkout,
+	LEAD(page_id) OVER(PARTITION BY visit_id ORDER BY sequence_number) AS purchase
+FROM events
+WHERE page_id IN ('12','13') 
+ORDER BY cookie_id,event_time)
+SELECT ROUND(100 * SUM(CASE WHEN checkout = 12 AND purchase IS NULL THEN 1 ELSE 0 END)/COUNT(DISTINCT(visit_id)),2) AS per_visits
+FROM visit_cte
 
 -- What are the top 3 pages by number of views?
-with pages_cte as (select event_name,
+WITH pages_cte AS (SELECT event_name,
 	page_name,
-	count(*) as count
-from events e inner join event_identifier ei on e.event_type = ei.event_type
-inner join page_hierarchy p on e.page_id = p.page_id
-where event_name = 'Page View' 
-group by event_name,page_name
-order by count desc
-limit 3)
-select page_name from pages_cte
+	COUNT(*) AS COUNT
+FROM events e INNER JOIN event_identifier ei ON e.event_type = ei.event_type
+INNER JOIN page_hierarchy p ON e.page_id = p.page_id
+WHERE event_name = 'Page View' 
+GROUP BY event_name,page_name
+ORDER BY COUNT DESC
+LIMIT 3)
+SELECT page_name FROM pages_cte
 
 -- What is the number of views and cart adds for each product category?
-select product_category,
-	sum(case when event_name = 'Page View' then 1 else 0 end) as page_views,
-    sum(case when event_name = 'Add to Cart' then 1 else 0 end) as add_to_cart
-from events e inner join event_identifier ei on e.event_type = ei.event_type
-inner join page_hierarchy p on e.page_id = p.page_id
-where product_category is not null
-group by product_category
+SELECT product_category,
+	sum(CASE WHEN event_name = 'Page View' THEN 1 else 0 END) AS page_views,
+    sum(CASE WHEN event_name = 'Add to Cart' THEN 1 else 0 END) AS add_to_cart
+FROM events e INNER JOIN event_identifier ei ON e.event_type = ei.event_type
+INNER JOIN page_hierarchy p ON e.page_id = p.page_id
+WHERE product_category IS NOT NULL
+GROUP BY product_category
 
 -- What are the top 3 products by purchases?
-select page_name,count(*) as count 
-from events e inner join event_identifier ei on e.event_type = ei.event_type
-inner join page_hierarchy p on e.page_id = p.page_id
-where event_name = 'Add to Cart' 
-	and product_id is not null
-    and visit_id in (select distinct visit_id from events where event_type = 3)
-group by page_name
-order by count desc
-limit 3
+SELECT page_name,COUNT(*) AS COUNT 
+FROM events e INNER JOIN event_identifier ei ON e.event_type = ei.event_type
+INNER JOIN page_hierarchy p ON e.page_id = p.page_id
+WHERE event_name = 'Add to Cart' 
+	AND product_id IS NOT NULL
+    AND visit_id IN (SELECT DISTINCT visit_id FROM events WHERE event_type = 3)
+GROUP BY page_name
+ORDER BY COUNT DESC
+LIMIT 3
 
--- Case Study 6 - Clique Bait/Product Funnel Analysis.sql
+++ Case Study 6 - Clique Bait/Product Funnel Analysis.sql
@@ -1,97 +1,97 @@
--- Using a single SQL query - create a new output table which has the following details:
+-- UsINg a sINgle SQL query - CREATE a new output TABLE which has the followINg details:
 -- How many times was each product viewed?
 -- How many times was each product added to cart?
--- How many times was each product added to a cart but not purchased (abandoned)?
+-- How many times was each product added to a cart but not purchased (abANDoned)?
 -- How many times was each product purchased?
-create TEMPORARY table product_info (
-with temp_table as (
-select visit_id,
+CREATE TEMPORARY TABLE product_info (
+WITH temp_table AS (
+SELECT visit_id,
 	p.page_id,
 	page_name,
 	event_type 
-from events e inner join page_hierarchy p on p.page_id = e.page_id
+FROM events e INNER JOIN page_hierarchy p ON p.page_id = e.page_id
 WHERE product_id IS NOT NULL
-group by visit_id,p.page_id,page_name,event_type),
-abandoned_cte as (
-select page_name,
-	count(distinct(visit_id)) as abandoned
-from temp_table
-where event_type = 2 
-	and visit_id not in (select distinct visit_id from events where event_type = 3)
-group by page_name),
-purchased_cte as (
-select page_name,
-	count(distinct(visit_id)) as purchased
-from temp_table
-where event_type = 2 
-	and visit_id in (select distinct visit_id from events where event_type = 3)
-group by page_name)
-select t.page_name,
-	sum(case when event_type = 1 then 1 else 0 end) as product_viewed,
-    sum(case when event_type = 2 then 1 else 0 end) as added_to_cart,
+GROUP BY visit_id,p.page_id,page_name,event_type),
+abandoned_cte AS (
+SELECT page_name,
+	COUNT(DISTINCT(visit_id)) AS abandoned
+FROM temp_TABLE
+WHERE event_type = 2 
+	AND visit_id NOT IN (SELECT DISTINCT visit_id FROM events WHERE event_type = 3)
+GROUP BY page_name),
+purchased_cte AS (
+SELECT page_name,
+	COUNT(DISTINCT(visit_id)) AS purchased
+FROM temp_table
+WHERE event_type = 2 
+	AND visit_id IN (SELECT DISTINCT visit_id FROM events WHERE event_type = 3)
+GROUP BY page_name)
+SELECT t.page_name,
+	SUM(CASE WHEN event_type = 1 THEN 1 ELSE 0 END) AS product_viewed,
+    SUM(CASE WHEN event_type = 2 THEN 1 ELSE 0 END) AS added_to_cart,
     abandoned,
     purchased
-from temp_table t left join abandoned_cte a on t.page_name = a.page_name
-	left join purchased_cte p on p.page_name = t.page_name
-group by t.page_name)
+FROM temp_table t LEFT JOIN abandoned_cte a ON t.page_name = a.page_name
+	LEFT JOIN purchased_cte p ON p.page_name = t.page_name
+GROUP BY t.page_name)
 
--- Additionally, create another table which further aggregates the data for the above points but this time for each product category instead of individual products.
-create temporary table product_category_info (
-with temp_table as (
-select visit_id,
+-- Additionally, CREATE another TABLE which further aggregates the data for the above poINts but this time for each product category INstead of INdividual products.
+CREATE TEMPORARY TABLE product_category_info (
+WITH temp_table AS (
+SELECT visit_id,
 	page_name,
     product_category,
 	event_type 
-from events e inner join page_hierarchy p on p.page_id = e.page_id
+FROM events e INNER JOIN page_hierarchy p ON p.page_id = e.page_id
 WHERE product_id IS NOT NULL
-group by visit_id,page_name,product_category,event_type),
-abandoned_cte as (
-select product_category,
-	count(visit_id) as abandoned
-from temp_table
-where event_type = 2 
-	and visit_id not in (select distinct visit_id from events where event_type = 3)
-group by product_category),
-purchased_cte as (
-select product_category,
-	count(visit_id) as purchased
-from temp_table
-where event_type = 2 
-	and visit_id in (select distinct visit_id from events where event_type = 3)
-group by product_category) 
-select t.product_category,
-	sum(case when event_type = 1 then 1 else 0 end) as product_viewed,
-    sum(case when event_type = 2 then 1 else 0 end) as added_to_cart,
+GROUP BY visit_id,page_name,product_category,event_type),
+abandoned_cte AS (
+SELECT product_category,
+	COUNT(visit_id) AS abandoned
+FROM temp_table
+WHERE event_type = 2 
+	AND visit_id NOT IN (SELECT DISTINCT visit_id FROM events WHERE event_type = 3)
+GROUP BY product_category),
+purchased_cte AS (
+SELECT product_category,
+	COUNT(visit_id) AS purchased
+FROM temp_table
+WHERE event_type = 2 
+	AND visit_id IN (SELECT DISTINCT visit_id FROM events WHERE event_type = 3)
+GROUP BY product_category) 
+SELECT t.product_category,
+	SUM(CASE WHEN event_type = 1 THEN 1 ELSE 0 END) AS product_viewed,
+    SUM(CASE WHEN event_type = 2 THEN 1 ELSE 0 END) AS added_to_cart,
     abandoned,
     purchased
-from temp_table t left join abandoned_cte a on t.product_category = a.product_category
-	left join purchased_cte p on p.product_category = t.product_category
-group by t.product_category)
+FROM temp_table t LEFT JOIN abandoned_cte a ON t.product_category = a.product_category
+	LEFT JOIN purchased_cte p ON p.product_category = t.product_category
+GROUP BY t.product_category)
 
--- Use your 2 new output tables - answer the following questions:
--- Which product had the most views, cart adds and purchases?
+-- Use your 2 new output TABLEs - answer the followINg questions:
+-- Which product had the most views, cart adds AND purchases?
 
-select page_name from product_info order by product_viewed desc limit 1;
-select page_name from product_info order by added_to_cart desc limit 1
-select page_name from product_info order by purchased desc limit 1
+SELECT page_name FROM product_info ORDER BY product_viewed DESC LIMIT 1;
+SELECT page_name FROM product_info ORDER BY added_to_cart DESC LIMIT 1
+SELECT page_name FROM product_info ORDER BY purchased DESC LIMIT 1
 
--- Which product was most likely to be abandoned?
-select page_name 
-from product_info 
-order by abandoned desc 
-limit 1
+-- Which product was most likely to be abANDoned?
+SELECT page_name 
+FROM product_info 
+ORDER BY abandoned DESC 
+LIMIT 1
 
 -- Which product had the highest view to purchase percentage?
-select page_name,
-	round(100*(purchased/product_viewed),2) as per 
-from product_info 
-order by per desc 
-limit 1 
+SELECT page_name,
+	ROUND(100*(purchased/product_viewed),2) AS per 
+FROM product_info 
+ORDER BY per DESC
+LIMIT 1 
 
--- What is the average conversion rate from view to cart add?
--- What is the average conversion rate from cart add to purchase?
+-- What is the average conversion rate FROM view to cart add?
+-- What is the average conversion rate FROM cart add to purchase?
 SELECT 
   ROUND(100*AVG(added_to_cart/product_viewed),2) AS avg_view_to_cart_add_conversion,
   ROUND(100*AVG(purchased/added_to_cart),2) AS avg_cart_add_to_purchases_conversion_rate
-FROM product_info
+FROM product_INfo
 
