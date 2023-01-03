edac6e563a36940e02eccac6006a4ef06853d269
PradnyaTipare <pradnyatipare04@gmail.com>
2023-01-03 15:25:10 -0700

    Update Product Analysis.sql

--- Case Study 7 - Balanced Tree Clothing Co/Product Analysis.sql
+++ Case Study 7 - Balanced Tree Clothing Co/Product Analysis.sql
@@ -1,88 +1,86 @@
 -- What are the top 3 products by total revenue before discount?
-select product_name,
-	sum(qty*s.price) as total_price 
-from sales s inner join product_details p on p.product_id = s.prod_id 
-group by product_name
-order by total_price desc
-limit 3
+SELECT product_name,
+	SUM(qty*s.price) AS total_price 
+FROM sales s INNER JOIN product_details p ON p.product_id = s.prod_id 
+GROUP BY product_name
+ORDER BY total_price DESC
+LIMIT 3
 
 -- What is the total quantity, revenue and discount for each segment?
-select segment_name,
-	sum(qty) as total_quantity,
-    sum(qty*s.price) as revenue,
-    round(sum((discount* qty*s.price)/100),2) as discount
-from sales s inner join product_details p on p.product_id = s.prod_id
-group by segment_name
+SELECT segment_name,
+	SUM(qty) AS total_quantity,
+    SUM(qty*s.price) AS revenue,
+    ROUND(SUM((discount* qty*s.price)/100),2) AS discount
+FROM sales s INNER JOIN product_details p ON p.product_id = s.prod_id
+GROUP BY segment_name
 
 -- What is the top selling product for each segment?
-select segment_name,
+SELECT segment_name,
 	product_name 
-from (select segment_name,
+FROM (SELECT segment_name,
 		product_name,
-        sum(qty) as quantity,
-        dense_rank() over(partition by segment_name order by sum(qty) desc) as rno
-from sales s inner join product_details p on p.product_id = s.prod_id
-group by segment_name,product_name) as rank_no
-where rno = 1
+        SUM(qty) AS quantity,
+        DENSE_RANK() OVER(PARTITION BY segment_name ORDER BY SUM(qty) DESC) AS rno
+FROM sales s INNER JOIN product_details p ON p.product_id = s.prod_id
+GROUP BY segment_name,product_name) AS rank_no
+WHERE rno = 1
 
 
 -- What is the total quantity, revenue and discount for each category?
-select category_name,
-	sum(qty) as total_quantity,
-    sum(qty*s.price) as revenue,
-    round(sum((discount* qty*s.price)/100),2) as discount
-from sales s inner join product_details p on p.product_id = s.prod_id
-group by category_name
+SELECT category_name,
+	SUM(qty) AS total_quantity,
+    SUM(qty*s.price) AS revenue,
+    ROUND(SUM((discount* qty*s.price)/100),2) AS discount
+FROM sales s INNER JOIN product_details p ON p.product_id = s.prod_id
+GROUP BY category_name
 
 -- What is the top selling product for each category?
-select category_name,
+SELECT category_name,
 	product_name 
-from (select category_name,
+FROM (SELECT category_name,
 			product_name,
-            sum(qty) as quantity,
-            dense_rank() over(partition by category_name order by sum(qty) desc) as rno
-from sales s inner join product_details p on p.product_id = s.prod_id
-group by category_name,product_name) as rank_no
-where rno = 1
+            SUM(qty) AS quantity,
+            DENSE_RANK() OVER(PARTITION BY category_name ORDER BY SUM(qty) DESC) AS rno
+FROM sales s INNER JOIN product_details p ON p.product_id = s.prod_id
+GROUP BY category_name,product_name) AS rank_no
+WHERE rno = 1
 
 -- What is the percentage split of revenue by product for each segment?
-with revenue_cte as (
-select segment_name,
+WITH revenue_cte AS (
+SELECT segment_name,
 	product_name,
-    sum(qty*s.price) as total_revenue
-from sales s inner join product_details p on p.product_id = s.prod_id
-group by segment_name,product_name)
-select segment_name,
+    SUM(qty*s.price) AS total_revenue
+FROM sales s INNER JOIN product_details p ON p.product_id = s.prod_id
+GROUP BY segment_name,product_name)
+SELECT segment_name,
 	product_name,
-    round((100 * total_revenue/sum(total_revenue) over(partition by segment_name)),2) as seg_product_per
-from revenue_cte
+    ROUND((100 * total_revenue/SUM(total_revenue) OVER(PARTITION BY segment_name)),2) AS seg_product_per
+FROM revenue_cte
 
 -- What is the percentage split of revenue by segment for each category?
-with revenue_cte1 as (
-select category_name,
+WITH revenue_cte1 AS (
+SELECT category_name,
 	segment_name,
-    sum(qty*s.price) as total_revenue
-from sales s inner join product_details p on p.product_id = s.prod_id
-group by category_name,segment_name)
-select category_name,
+    SUM(qty*s.price) AS total_revenue
+FROM sales s INNER JOIN product_details p ON p.product_id = s.prod_id
+GROUP BY category_name,segment_name)
+SELECT category_name,
 	segment_name,
-    round((100 * total_revenue/sum(total_revenue) over(partition by category_name)),2) as category_seg_per
-from revenue_cte1
+    ROUND((100 * total_revenue/SUM(total_revenue) OVER(PARTITION BY category_name)),2) AS category_seg_per
+FROM revenue_cte1
 
 -- What is the percentage split of total revenue by category?
-with revenue_cte2 as (
-select category_name,
-    sum(qty*s.price) as total_revenue
-from sales s inner join product_details p on p.product_id = s.prod_id
-group by category_name)
-select category_name,
-    round((100 * total_revenue/sum(total_revenue) over()),2) as category_per
-from revenue_cte2
+WITH revenue_cte2 AS (
+SELECT category_name,
+    SUM(qty*s.price) AS total_revenue
+FROM sales s INNER JOIN product_details p ON p.product_id = s.prod_id
+GROUP BY category_name)
+SELECT category_name,
+    ROUND((100 * total_revenue/SUM(total_revenue) OVER()),2) AS category_per
+FROM revenue_cte2
 
--- What is the total transaction “penetration” for each product? (hint: penetration = number of transactions where at least 1 quantity of a product was purchased divided by total number of transactions)
-select prod_id,
-	round((100*count(case when qty>=1 then txn_id else null end)/(select count(distinct(txn_id)) from sales)),2) 
-from sales 
-group by prod_id
-
--- What is the most common combination of at least 1 quantity of any 3 products in a 1 single transaction?
+-- What is the total transaction “penetration” for each product? (hint: penetration = number of transactions WHERE at least 1 quantity of a product was purchased divided by total number of transactions)
+SELECT prod_id,
+	ROUND((100*COUNT(CASE WHEN qty>=1 THEN txn_id ELSE NULL END)/(SELECT COUNT(DISTINCT(txn_id)) FROM sales)),2) 
+FROM sales 
+GROUP BY prod_id
